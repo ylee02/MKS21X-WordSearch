6ecc7cce5d2f50c8f363291051d420ae3948cbc4
ylee02 <ylee01@stuy.edu>
2018-11-14 13:31:48 +0900

    Added new constructors

--- WordSearch.java
+++ WordSearch.java
@@ -7,12 +7,6 @@ public class WordSearch{
 	private Random randgen;
 	private ArrayList<String> wordsToAdd;
 	private ArrayList<String> wordsAdded;
-    /**Initialize the grid to the size specified 
-     *and fill all of the positions with '_'
-     *@param row is the starting height of the WordSearch
-     *@param col is the starting width of the WordSearch
-     */
-	
     /*public WordSearch(int rows,int cols){
 		if (rows <= 0 || cols <= 0) {
 			throw new IllegalArgumentException("Indices cannot be negative");
@@ -20,11 +14,52 @@ public class WordSearch{
 		data = new char[rows][cols];
 		clear();
     }
-	*/
+	
+
+    /*[rowIncrement,colIncrement] examples:
+     *[-1,1] would add up and the right because (row -1 each time, col + 1 each time)
+     *[ 1,0] would add downwards because (row+1), with no col change
+     *[ 0,-1] would add towards the left because (col - 1), with no row change
+     */
+	
 	public WordSearch(int rows, int cols, String fileName) {
+		if (rows <= 0 || cols <= 0) {
+			throw new IllegalArgumentException("Nonpositive integers are not allowed")
+		}
+		wordsToAdd = new ArrayList<String>();
+		wordsAdded = new ArrayList<String>();
+		data = new char[rows][cols];
+		clear();
+		randgen = new Random();
+		File fil = new File(fileName);
+		Scanner file = new Scanner(fil);
+		while (file.hasNext()){
+			String temp = file.next();
+			wordsToAdd.add(temp.toUpperCase());
+		}
+		addAllWords();
+	}
+			
+		
+		
 	}
 	
 	public WordSearch( int rows, int cols, String fileName, int randSeed) {
+		if (rows <= 0 || cols <= 0) {
+			throw new IllegalArgumentException("Nonpositive integers are not allowed")
+		}
+		wordsToAdd = new ArrayList<String>();
+		wordsAdded = new ArrayList<String>();
+		data = new char[rows][cols];
+		clear();
+		randgen = new Random(randSeed);
+		File fil = new File(fileName);
+		Scanner file = new Scanner(fil);
+		while (file.hasNext()){
+			String temp = file.next();
+			wordsToAdd.add(temp.toUpperCase());
+		}
+		addAllWords();
 	}
 
     /**Set all values in the WordSearch to underscores'_'*/
@@ -35,14 +70,10 @@ public class WordSearch{
 			}
 		}
     }
-
-    /**Each row is a new line, there is a space between each letter
-     *@return a String with each character separated by spaces, and rows
-     *separated by newlines.
-     */
     public String toString(){
 		String ans = "";
 		for (int i = 0; i < data.length; i++) {
+			ans += "|";
 			for (int x = 0; x < data[i].length; x++) {
 				if (x == data[i].length - 1) {
 					ans += data[i][x];
@@ -51,12 +82,15 @@ public class WordSearch{
 					ans += data[i][x] + " ";
 				}
 			}
+			ans += "|";
 			ans += "\n";
 		}	
 		return ans;
     }
 	
-	/*private char[][] copyWord(char[][] ary) {
+
+	
+	private static char[][] copyWord(char[][] ary) {
 		char[][] ans = new char[ary.length][ary[0].length];
 		for (int i = 0; i < data.length; i++) {
 			for (int x = 0; x < data[i].length; x++) {
@@ -65,21 +99,29 @@ public class WordSearch{
 		}
 		return ans;
 	}
-	*/
 	
 
-
-    /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from left to right, must fit on the WordGrid, and must
-     *have a corresponding letter to match any letters that it overlaps.
+	 /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added in the direction rowIncrement,colIncrement 
+     *Words must have a corresponding letter to match any letters that it overlaps.
      *
      *@param word is any text to be added to the word grid.
      *@param row is the vertical locaiton of where you want the word to start.
      *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     * or there are overlapping letters that do not match, then false is returned 
-     * and the board is NOT modified.
+     *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
+     *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
+     *@return true when: the word is added successfully. 
+     *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
+     *        OR there are overlapping letters that do not match
      */
+    private boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
+		char[][] ans = copyWord(data);
+		
+    }
+	
+
+
+
    /* public boolean addWordHorizontal(String word,int row, int col){
 		if (row < 0 || col < 0 || row >= data.length || col+word.length() > data[row].length) {
 			return false;
@@ -99,17 +141,7 @@ public class WordSearch{
 		
     }
 */
-   /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from top to bottom, must fit on the WordGrid, and must
-     *have a corresponding letter to match any letters that it overlaps.
-     *
-     *@param word is any text to be added to the word grid.
-     *@param row is the vertical locaiton of where you want the word to start.
-     *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     *or there are overlapping letters that do not match, then false is returned.
-     *and the board is NOT modified.
-     */
+ 
     /*public boolean addWordVertical(String word,int row, int col){
 		if (row < 0 || col < 0 || row+word.length() > data.length || col >= data[row].length) {
 			return false;
